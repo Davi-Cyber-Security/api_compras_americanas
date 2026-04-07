92993677cf29c5abb2f229118248b1d038e2e0ce
Davi-Cyber-Security <davigabriell442@gmail.com>
2026-04-07 20:27:32 -0300

    Fazendo correção na porta da API

--- src/index.js
+++ src/index.js
@@ -51,9 +51,17 @@ app.get('/', (req, res) => {
     res.send("");
 })
 
+app.all("/", (req, res, next) => {
+    if(req.method !== 'GET'){
+        res.status(405).send("Método não permitido.");
+    } else{
+        next();
+    }
+})
+
 export function startServer() {
     const PORT = process.env.PORT || 8080;
-    app.listen(3004, '0.0.0.0', () => {
+    app.listen(PORT, '0.0.0.0', () => {
         console.log(`Servidor rodando na porta ${PORT}`);
         startScheduler();
     });
